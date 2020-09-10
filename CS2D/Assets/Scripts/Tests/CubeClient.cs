3f2a05cf6428efffcec31fdc1b926ad621f5d554
tferrerITBA <tferrer@itba.edu.ar>
2020-09-09 21:07:55 -0300

    Create CubeClient script to start supporting multiple clients (breaking change)

---
+++ CS2D/Assets/Scripts/Tests/CubeClient.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using Tests;
+using UnityEngine;
+
+public class CubeClient : MonoBehaviour
+{
+    public int sendPort;
+    public int recvPort;
+    public Channel sendChannel;
+    public Channel recvChannel;
+
+    public int userID;
+    public int displaySeq = 0;
+    public float time = 0;
+    public bool isPlaying;
+
+    public List<Snapshot> interpolationBuffer = new List<Snapshot>();
+    public List<Commands> commands = new List<Commands>();
+    public int minBufferElems;
+
+    public void Initialize(int sendPort, int recvPort, int userID, int minBufferElems)
+    {
+        this.sendPort = sendPort;
+        this.sendChannel = new Channel(sendPort);
+        this.recvPort = recvPort;
+        this.recvChannel = new Channel(recvPort);
+        this.userID = userID;
+        this.minBufferElems = minBufferElems;
+    }
+
+    private void Update()
+    {
+        var packet = recvChannel.GetPacket();
+
+        if (packet != null) {
+            var buffer = packet.buffer;
+
+            //deserialize
+            CubeEntity.ClientDeserialize(interpolationBuffer, buffer, displaySeq, commands);
+            //networkSeq++;
+        }
+
+        ReadClientInput();
+
+        if (interpolationBuffer.Count >= minBufferElems)
+            isPlaying = true;
+        else if (interpolationBuffer.Count <= 1)
+            isPlaying = false;
+        
+        if (isPlaying)
+        {
+            //accumCli += Time.deltaTime;
+            time += Time.deltaTime;
+            var previousTime = interpolationBuffer[0].Time;
+            var nextTime = interpolationBuffer[1].Time;
+            if (time >= nextTime) {
+                interpolationBuffer.RemoveAt(0);
+                displaySeq++;
+                if (interpolationBuffer.Count < 2)
+                {
+                    isPlaying = false;
+                    return;
+                }
+                previousTime = interpolationBuffer[0].Time;
+                nextTime =  interpolationBuffer[1].Time;
+            }
+            var t =  (time - previousTime) / (nextTime - previousTime);
+            Interpolate(interpolationBuffer[0], interpolationBuffer[1], t);
+        }
+    }
+    
+    private void ReadClientInput()
+    {
+        Commands currentCommands = new Commands(
+            Input.GetKeyDown(KeyCode.UpArrow),
+            Input.GetKeyDown(KeyCode.DownArrow),
+            Input.GetKeyDown(KeyCode.RightArrow),
+            Input.GetKeyDown(KeyCode.LeftArrow),
+            Input.GetKeyDown(KeyCode.Space)
+        );
+        
+        if (currentCommands.hasCommand())
+        {
+            commands.Add(currentCommands);
+            //serialize
+            var packet = Packet.Obtain();
+            CubeEntity.ClientSerializeInput(commands, packet.buffer);
+            packet.buffer.Flush();
+
+            string serverIP = "127.0.0.1";
+            var remoteEp = new IPEndPoint(IPAddress.Parse(serverIP), sendPort);
+            sendChannel.Send(packet, remoteEp);
+
+            packet.Free();
+        }
+    }
+    
+    private void Interpolate(Snapshot prevSnapshot, Snapshot nextSnapshot, float t)
+    {
+        //Debug.Log(prevSnapshot + " " + nextSnapshot);
+        var position = new Vector3();
+        var rotation = new Quaternion();
+
+        position.x = InterpolateAxis(prevSnapshot.Position.x, nextSnapshot.Position.x, t);
+        position.y = InterpolateAxis(prevSnapshot.Position.y, nextSnapshot.Position.y, t);
+        position.z = InterpolateAxis(prevSnapshot.Position.z, nextSnapshot.Position.z, t);
+    
+        rotation.w = InterpolateAxis(prevSnapshot.Rotation.w, nextSnapshot.Rotation.w, t);
+        rotation.x = InterpolateAxis(prevSnapshot.Rotation.x, nextSnapshot.Rotation.x, t);
+        rotation.y = InterpolateAxis(prevSnapshot.Rotation.y, nextSnapshot.Rotation.y, t);
+        rotation.z = InterpolateAxis(prevSnapshot.Rotation.z, nextSnapshot.Rotation.z, t);
+    
+        transform.position = position;
+        transform.rotation = rotation;
+    }
+
+    private float InterpolateAxis(float currentSnapValue, float nextSnapValue, float t)
+    {
+        return currentSnapValue + (nextSnapValue - currentSnapValue) * t;
+    }
+    
+    private void OnDestroy() {
+        sendChannel.Disconnect();
+        recvChannel.Disconnect();
+    }
+}
--- CS2D/Assets/Scripts/Tests/SimulationTest.cs
+++ CS2D/Assets/Scripts/Tests/SimulationTest.cs
@@ -6,15 +6,22 @@ using System.Net;
 using Tests;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.Serialization;
+using Random = System.Random;
 
 public class SimulationTest : MonoBehaviour
 {
 
-    private Channel channel;
-    private Channel clientChannel;
+    public GameObject clientPrefab;
 
     [SerializeField] private Rigidbody cubeRigidBody;
-    [SerializeField] private Transform clientCubeTransform;
+
+    [SerializeField] private Dictionary<int, CubeClient> cubeClients = new Dictionary<int, CubeClient>();
+
+    public const int PortsPerClient = 2;
+    public int sendBasePort = 9000;
+    public int recvBasePort = 9001;
+    public int clientCount = 0;
     
     public int pps = 10;
     private float sendRate;
@@ -22,135 +29,89 @@ public class SimulationTest : MonoBehaviour
     private float accum = 0;
     private float serverTime = 0;
     private int seq = 0; // Next snapshot to send
-
-    private bool clientPlaying = false;
-    public int interpolationCount = 3;
-    private int displaySeq = 0; // Wait for buffer to fill before changing
-    private Snapshot currentSnapshot;
-    private float clientTime = 0;
-
-    private List<Snapshot> interpolationBuffer;
-
-    private List<Commands> clientCommands = new List<Commands>();
-
-    private bool connected = true;
+    private bool serverConnected;
+    
+    public int interpolationCount = 2;
 
     // Start is called before the first frame update
     void Start() {
-        channel = new Channel(9000);
-        clientChannel = new Channel(9001);
         sendRate = 1f / pps;
-        interpolationBuffer = new List<Snapshot>();
-    }
-
-    private void OnDestroy() {
-        channel.Disconnect();
     }
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            int userID = new Random().Next();
+            GameObject userCube = Instantiate(clientPrefab);
+            CubeClient cubeClientComponent = userCube.GetComponent<CubeClient>();
+            cubeClientComponent.Initialize(recvBasePort + clientCount * PortsPerClient,
+                sendBasePort + clientCount * PortsPerClient, userID, interpolationCount);
+            clientCount++;
+        }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            connected = !connected;
+            serverConnected = !serverConnected;
         }
 
         accum += Time.deltaTime;
 
-        if (connected)
+        if (serverConnected)
         {
             UpdateServer();
         }
-
-
-        UpdateClient();
     }
 
     private void UpdateServer()
     {
         serverTime += Time.deltaTime;
-        
-        var commandPacket = clientChannel.GetPacket();
-        if (commandPacket != null) {
-            var buffer = commandPacket.buffer;
 
-            List<Commands> commandsList = CubeEntity.ServerDeserializeInput(buffer);
-            var packet = Packet.Obtain();
-            int receivedCommandSequence = -1;
-            foreach (Commands commands in commandsList)
-            {
-                receivedCommandSequence = commands.Seq;
-                ExecuteClientInput(commands);
-            }
-            CubeEntity.ServerSerializeAck(packet.buffer, receivedCommandSequence);
-            packet.buffer.Flush();
-
-            string serverIP = "127.0.0.1";
-            int port = 9000;
-            var remoteEp = new IPEndPoint(IPAddress.Parse(serverIP), port);
-            channel.Send(packet, remoteEp);
-
-            packet.Free();
-        }
-
-        if (accum >= sendRate)
+        foreach (var cubeClientPair in cubeClients)
         {
-            //serialize
-            var packet = Packet.Obtain();
-            CubeEntity.ServerWorldSerialize(cubeRigidBody, packet.buffer, seq, serverTime);
-            packet.buffer.Flush();
+            int userID = cubeClientPair.Key;
+            CubeClient cubeClient = cubeClientPair.Value;
+            var commandPacket = cubeClient.sendChannel.GetPacket();
+            
+            if (commandPacket != null) {
+                var buffer = commandPacket.buffer;
 
-            string serverIP = "127.0.0.1";
-            int port = 9000;
-            var remoteEp = new IPEndPoint(IPAddress.Parse(serverIP), port);
-            channel.Send(packet, remoteEp);
-
-            packet.Free();
-
-            accum -= sendRate;
-            seq++;
-        }
-    }
-
-    private void UpdateClient() {
-        var packet = channel.GetPacket();
-
-        if (packet != null) {
-            var buffer = packet.buffer;
-
-            //deserialize
-            CubeEntity.ClientDeserialize(interpolationBuffer, buffer, displaySeq, clientCommands);
-            //networkSeq++;
-        }
-
-        ReadClientInput();
-
-        if (interpolationBuffer.Count >= interpolationCount)
-            clientPlaying = true;
-        else if (interpolationBuffer.Count <= 1)
-            clientPlaying = false;
-        
-        if (clientPlaying)
-        {
-            //accumCli += Time.deltaTime;
-            clientTime += Time.deltaTime;
-            var previousTime = interpolationBuffer[0].Time;
-            var nextTime = interpolationBuffer[1].Time;
-            if (clientTime >= nextTime) {
-                interpolationBuffer.RemoveAt(0);
-                displaySeq++;
-                if (interpolationBuffer.Count < 2)
+                List<Commands> commandsList = CubeEntity.ServerDeserializeInput(buffer);
+                var packet = Packet.Obtain();
+                int receivedCommandSequence = -1;
+                foreach (Commands commands in commandsList)
                 {
-                    clientPlaying = false;
-                    return;
+                    receivedCommandSequence = commands.Seq;
+                    ExecuteClientInput(commands);
                 }
-                previousTime = interpolationBuffer[0].Time;
-                nextTime =  interpolationBuffer[1].Time;
+                CubeEntity.ServerSerializeAck(packet.buffer, receivedCommandSequence);
+                packet.buffer.Flush();
+
+                string serverIP = "127.0.0.1";
+                var remoteEp = new IPEndPoint(IPAddress.Parse(serverIP), cubeClient.recvPort);
+                cubeClient.recvChannel.Send(packet, remoteEp);
+
+                packet.Free();
             }
-            var t =  (clientTime - previousTime) / (nextTime - previousTime);
-            Interpolate(interpolationBuffer[0], interpolationBuffer[1], t);
+            
+            if (accum >= sendRate)
+            {
+                //serialize
+                var packet = Packet.Obtain();
+                CubeEntity.ServerWorldSerialize(cubeRigidBody, packet.buffer, seq, serverTime);
+                packet.buffer.Flush();
+
+                string serverIP = "127.0.0.1";
+                var remoteEp = new IPEndPoint(IPAddress.Parse(serverIP), cubeClient.recvPort);
+                cubeClient.recvChannel.Send(packet, remoteEp);
+
+                packet.Free();
+
+                accum -= sendRate;
+                seq++;
+            }
         }
     }
-
+    
     private void ExecuteClientInput(Commands commands)
     {
         //apply input
@@ -169,56 +130,5 @@ public class SimulationTest : MonoBehaviour
         if (commands.Down) {
             cubeRigidBody.AddForceAtPosition(Vector3.back * 5, Vector3.zero, ForceMode.Impulse);
         }
-    }
-
-    private void ReadClientInput()
-    {
-        Commands currentCommands = new Commands(
-            Input.GetKeyDown(KeyCode.UpArrow),
-            Input.GetKeyDown(KeyCode.DownArrow),
-            Input.GetKeyDown(KeyCode.RightArrow),
-            Input.GetKeyDown(KeyCode.LeftArrow),
-            Input.GetKeyDown(KeyCode.Space)
-            );
-        
-        if (currentCommands.hasCommand())
-        {
-            clientCommands.Add(currentCommands);
-            //serialize
-            var packet = Packet.Obtain();
-            CubeEntity.ClientSerializeInput(clientCommands, packet.buffer);
-            packet.buffer.Flush();
-
-            string serverIP = "127.0.0.1";
-            int port = 9001;
-            var remoteEp = new IPEndPoint(IPAddress.Parse(serverIP), port);
-            channel.Send(packet, remoteEp);
-
-            packet.Free();
-        }
-    }
-
-    private void Interpolate(Snapshot prevSnapshot, Snapshot nextSnapshot, float t)
-    {
-        //Debug.Log(prevSnapshot + " " + nextSnapshot);
-        var position = new Vector3();
-        var rotation = new Quaternion();
-
-        position.x = InterpolateAxis(prevSnapshot.Position.x, nextSnapshot.Position.x, t);
-        position.y = InterpolateAxis(prevSnapshot.Position.y, nextSnapshot.Position.y, t);
-        position.z = InterpolateAxis(prevSnapshot.Position.z, nextSnapshot.Position.z, t);
-        
-        rotation.w = InterpolateAxis(prevSnapshot.Rotation.w, nextSnapshot.Rotation.w, t);
-        rotation.x = InterpolateAxis(prevSnapshot.Rotation.x, nextSnapshot.Rotation.x, t);
-        rotation.y = InterpolateAxis(prevSnapshot.Rotation.y, nextSnapshot.Rotation.y, t);
-        rotation.z = InterpolateAxis(prevSnapshot.Rotation.z, nextSnapshot.Rotation.z, t);
-        
-        clientCubeTransform.position = position;
-        clientCubeTransform.rotation = rotation;
-    }
-
-    private float InterpolateAxis(float currentSnapValue, float nextSnapValue, float t)
-    {
-        return currentSnapValue + (nextSnapValue - currentSnapValue) * t;
     }
 }
