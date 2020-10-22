2e1b8da7d4189090be0b3c94d64facb5bbfdec98
tferrerITBA <tferrer@itba.edu.ar>
2020-10-22 16:58:07 -0300

    Remove commands from list now

--- CS2D/Assets/Scripts/Tests/CommandsList.cs
+++ CS2D/Assets/Scripts/Tests/CommandsList.cs
@@ -12,9 +12,10 @@ public class CommandsList
 
     public void ClearAcked()
     {
-        return;
-        commands.RemoveRange(0, ackedIndex + 1);
-        ackedIndex = -1;
+        int minIndex = Math.Min(ackedIndex, snapshotAckedIndex);
+        commands.RemoveRange(0, minIndex + 1);
+        ackedIndex -= minIndex + 1;
+        snapshotAckedIndex -= minIndex + 1;
     }
 
     public List<Commands> GetUnackedCommands()
@@ -23,10 +24,8 @@ public class CommandsList
             return commands;
 
         if (commands.Count < ackedIndex + 1)
-        {
             return new List<Commands>(0);
-        }
-           
+
         return commands.GetRange(ackedIndex + 1, commands.Count - ackedIndex - 1);
     }
 
@@ -35,10 +34,8 @@ public class CommandsList
         if (snapshotAckedIndex < 0)
             return commands;
         if (commands.Count < snapshotAckedIndex + 1)
-        {
-            Debug.Log(commands.Count);
             return new List<Commands>(0);
-        }
+
         return commands.GetRange(snapshotAckedIndex + 1, commands.Count - snapshotAckedIndex - 1);
     }
 
